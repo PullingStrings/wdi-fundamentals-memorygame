458e8248f7ffd616f198f330e3d143e77d19781f
PullingStrings <drammatik@gmail.com>
2017-07-17 09:03:53 +0100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("up and running!");
+var cardOne = "queen of diamonds"
+var cardTwo = "queen of hearts"
+var cardThree = "king of diamounds"
+var cardFour =  "king of hearts"
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);

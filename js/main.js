0f298d79d4bb3e17624242ecc06c706eef333d69
PullingStrings <drammatik@gmail.com>
2017-07-20 19:38:02 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 console.log("up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+ {
+ 	rank: "queen",
+ 	suit: "diamonds",
+ 	cardImage: "images/queen-of-hearts.png",
+ },
+ {
+ 	rank: "queen", 
+ 	suit: "diamonds",
+ 	cardImage: "images/queen-of-diamonds.png",
+ },
+ {
+ 	rank: "king",
+ 	suit: "hearts",
+ 	cardImage: "images/king-of-hearts.png",
+ },
+ {
+ 	rank: "king", 
+ 	suit: "diamonds",
+ 	cardImage: "images/king-of-diamonds.png",
+ },
+ ];
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if (cardsInPlay.length === 2){
@@ -14,8 +35,10 @@ var checkForMatch = function(){
 	}
 }
 var flipCard = function(cardID){
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].suit);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 
 checkForMatch();
 

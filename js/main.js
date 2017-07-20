6d650651564d6545dd0fd8576491991852631a26
PullingStrings <drammatik@gmail.com>
2017-07-20 12:42:19 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,16 +2,8 @@ console.log("up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
 	alert("You have found a match!");
     }
 	else if (cardsInPlay[0] === cardsInPlay[1]){
@@ -20,4 +12,13 @@ if (cardsInPlay.length === 2){
 	else {
 		alert("Sorry, try again");
 	}
+}
+var flipCard = function(cardID){
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
 
+checkForMatch();
+
+}
+flipCard(0);
+flipCard(2);

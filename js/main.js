d57f4c2f56b3fbcd86d4c5bb2b844753fb439d1b
PullingStrings <drammatik@gmail.com>
2017-07-25 19:09:10 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log("up and running!");
 var cards = [
  {
  	rank: "queen",
- 	suit: "diamonds",
+ 	suit: "hearts",
  	cardImage: "images/queen-of-hearts.png",
  },
  {
@@ -24,24 +24,38 @@ var cards = [
  ];
 var cardsInPlay = [];
 var checkForMatch = function(){
+	
 	if (cardsInPlay.length === 2){
 	alert("You have found a match!");
     }
-	else if (cardsInPlay[0] === cardsInPlay[1]){
+	else if (cardsInPlay[0] === cardsInPlay[2]){
 		alert("You have found a match!");
 	}
 	else {
 		alert("Sorry, try again");
 	}
 }
-var flipCard = function(cardID){
+var flipCard = function(){
+	var cardID = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardID].rank);
 	cardsInPlay.push(cards[cardID].suit);
 	console.log(cards[cardID].cardImage);
 	console.log(cards[cardID].suit);
 
+this.setAttribute("src", cards[cardID].cardImage);
 checkForMatch();
 
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	document.getElementById("game-board")
+
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
